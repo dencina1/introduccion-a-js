ec2ccab0fc94b4532ee693ef2fb6df2eea7a5c3a
dencina1 <dencina@gmail.com>
2021-01-24 21:31:02 -0300

    Completed level 2

--- js/nivel2.js
+++ js/nivel2.js
@@ -292,14 +292,23 @@ for (i = 10; i > 0; i--) {
 // TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
 //tarea
 
+function parte1() {
+    for (let i=3; i<=22; i+=3){
+        console.log (i);
+    }
+    
+}
+parte1();
+
+
 //Ejercicio
-const notaParciales = [10, 4, 5, 6];
+/* const notaParciales = [10, 4, 5, 6];
 let totalNotas = 0;
 for (let i=0; i < notaParciales.length; i++){
     totalNotas = totalNotas + notaParciales[i];
 }
 
-console.log ('El promedio es ' + totalNotas / notaParciales.length)
+console.log ('El promedio es ' + totalNotas / notaParciales.length) */
 
 
 
@@ -354,6 +363,17 @@ console.log ('El promedio es ' + totalNotas / notaParciales.length)
     }
 */
 
+//TAREA: Usando un buvle 'while', decile a tu computadora que registre los numeros de 10 a 1
+/* function tarea2(){
+    let i = 11; 
+    while (i>1){
+        i--; 
+        console.log(i);
+    }
+}
+ */
+
+
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
 
 // Cuenta del 1 al 50 e imprime los números:
@@ -370,10 +390,38 @@ console.log ('El promedio es ' + totalNotas / notaParciales.length)
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
 
+/* 
+function fizzBuzzClasico(){
+    for (let i = 1; i<=50; i++){
+        if (i % 3 === 0 && i % 5 === 0 ){
+            console.log ('FizzBuzz');
+        } else if (i % 3 === 0){
+            console.log ('Fizz');
+        } else if (i % 5 === 0){
+            console.log ('Buzz');
+        } else {
+            console.log (i);
+        }
+    }
+}
+fizzBuzz()  */
 
+function fizzBuzz (){
+    for (let i = 1; i<=50; i++){
+    let texto = '';
 
+    if(i % 3 === 0){
+        texto = 'Fizz';
+    }
 
+    if (i% 5 === 0){
+        texto += 'Buzz';
+    }
 
+    console.log(texto || i);
+    }
+}
+fizzBuzz();
 
 //////////////////////////////////////////////////////////////////////////////
 // ¡Felicidades! ¡Has terminado el Nivel 2 de JavaScript Básico!            //
